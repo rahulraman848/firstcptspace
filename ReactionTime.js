808c261bef14469c16226de502b7acb3d92dd3ee
ramanrahul788 <ramanrahul788@gmail.com>
2022-11-27 10:52:54 -0800

    format

--- ReactionTime.js
+++ ReactionTime.js
@@ -1,70 +1,71 @@
-const button = document.getElementById('myButton');
-button.addEventListener('click', remove);
-const shape = document.getElementById('shape');
-shape.style.visibility = 'hidden';
+const button = document.getElementById("myButton");
+button.addEventListener("click", remove);
+const shape = document.getElementById("shape");
+shape.style.visibility = "hidden";
 let firstDate = 0;
-const playAgain = document.getElementById('playAgain');
-playAgain.style.visibility = 'hidden';
+const playAgain = document.getElementById("playAgain");
+playAgain.style.visibility = "hidden";
 let reactionTimes = [];
 let attempts = 0;
-let sum  = 0;
+let sum = 0;
 
 function remove() {
-    button.style.display = 'none';
+  button.style.display = "none";
 }
 
-function doIt(){
-    if(attempts < 5){
-        playAgain.style.visibility = 'hidden';
-        let time = document.querySelector('h1');
-        let seconds = 3;
+function doIt() {
+  if (attempts < 5) {
+    playAgain.style.visibility = "hidden";
+    let time = document.querySelector("h1");
+    let seconds = 3;
 
-        time.innerHTML = seconds;
+    time.innerHTML = seconds;
 
-        let countdown = setInterval(clock, 1000);
+    let countdown = setInterval(clock, 1000);
 
-        function clock(){
-            let topVal = getRandValue(0, window.outerHeight);
-            let leftVal = getRandValue(0, window.outerWidth);
-            seconds --;
-            time.innerHTML = seconds;
-            if(seconds==0){
-                clearInterval(countdown);
-                time.innerHTML = "Go!"
-                setTimeout(() =>{
-                    time.innerHTML = "";
-                },700);
-                setTimeout(() =>{
-                    shape.style.visibility = 'visible';
-                    shape.style.marginTop = topVal + "px";
-                    shape.style.marginLeft = leftVal + "px";
-                    firstDate = new Date();
-                },700);
-                return;
-            }
-        }
+    function clock() {
+      let topVal = getRandValue(0, window.outerHeight);
+      let leftVal = getRandValue(0, window.outerWidth);
+      seconds--;
+      time.innerHTML = seconds;
+      if (seconds == 0) {
+        clearInterval(countdown);
+        time.innerHTML = "Go!";
+        setTimeout(() => {
+          time.innerHTML = "";
+        }, 700);
+        setTimeout(() => {
+          shape.style.visibility = "visible";
+          shape.style.marginTop = topVal + "px";
+          shape.style.marginLeft = leftVal + "px";
+          firstDate = new Date();
+        }, 700);
+        return;
+      }
     }
-    else {
-        for(let i=0; i<reactionTimes.length; i++){
-            sum += reactionTimes[i];
-        }
-        document.write(
-            "Your 5 scores were " + reactionTimes + "<br> <br> Your average time is " + sum/5 + " ms"
-        )
+  } else {
+    for (let i = 0; i < reactionTimes.length; i++) {
+      sum += reactionTimes[i];
     }
+    document.write(
+      "Your 5 scores were " +
+        reactionTimes +
+        "<br> <br> Your average time is " +
+        sum / 5 +
+        " ms"
+    );
+  }
 }
-function getRandValue(min, max)
-{
-   return Math.random() * (max-min) + min;
+function getRandValue(min, max) {
+  return Math.random() * (max - min) + min;
 }
-function reaction()
-{
-    attempts ++;
-    secondDate = new Date();
-    reactionTimes.push(secondDate - firstDate);
-    shape.style.visibility = 'hidden';
-    if(attempts == 5){
-        playAgain.innerText = "Reveal your scores";
-    }
-    playAgain.style.visibility = 'visible';
+function reaction() {
+  attempts++;
+  secondDate = new Date();
+  reactionTimes.push(secondDate - firstDate);
+  shape.style.visibility = "hidden";
+  if (attempts == 5) {
+    playAgain.innerText = "Reveal your scores";
+  }
+  playAgain.style.visibility = "visible";
 }
